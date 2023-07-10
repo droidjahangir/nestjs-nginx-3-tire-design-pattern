b61d411089295ba6b7ca1900fd72ebf5fb5aa9a0
jahangir <jahangir.droid@gmail.com>
2023-07-10 23:18:39 +0600

    docker and compose file updated

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(APIPrefix.Version);
-  const port = parseInt(process.env.SERVER_PORT);
+  const port = parseInt(process.env.NODE_PORT) || parseInt(process.env.SERVER_PORT);
   console.log('port ----> ', port)
   await app.listen(port);
 }
